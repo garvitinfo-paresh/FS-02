ff1abab43c087420fed31b1c8f9013f438748656
PareshRajput <garvitinfo.paresh@gmail.com>
2026-08-08 22:39:23 +0530

    08-08-2026_1

--- SQL/manufacturing.sql
+++ SQL/manufacturing.sql
@@ -6,7 +6,9 @@
  
 --  1) create Database 
       
---       CREATE DATABASE manufacturing_db;
+      CREATE DATABASE manufacturing_db;
+      USE manufacturing_db;
+
 
 -- 2) creating Tables
 
@@ -28,7 +30,7 @@
       production_line VARCHAR(50),
       idle_time DECIMAL(5,2),
       machine_install_date DATE
-      )
+      );
 
     --   production table
     --   ---------------
@@ -40,9 +42,9 @@
       produciton_date DATE,
       units_produced INT,
       units_defective INT,
-      production_duration DECIMAL(5,2)
+      production_duration DECIMAL(5,2),
       CONSTRAINT FK_Product FOREIGN KEY (product_id) REFERENCES products(product_id),
-      CONSTRAINT FK_Machine_production FOREIGN KEY (machine_id) REFERENCES machine(machine_id),
+      CONSTRAINT FK_Machine_Production FOREIGN KEY (machine_id) REFERENCES machine(machine_id)
       );
 
     --   maintainance table
@@ -53,13 +55,36 @@
       machine_id INT,
       maintainance_date DATE,
       maintainance_desc VARCHAR(75),
-      halt_time decimal(5,2);
+      halt_time decimal(5,2),
       maintainance_cost decimal(9,2),
-      CONSTRAINT FK_Machine_Maintainance FOREIGN KEY (machine_id) REFERENCES machine(machine_id),
-      )
+      CONSTRAINT FK_Machine_Maintainance FOREIGN KEY (machine_id) REFERENCES machine(machine_id)
+      );
 
-    --  bottel - > 1 hr -> 180
-    --  daily - > 24 hr -> 4320
-    --  monthly - > 24 hr -> 129600  
-                         --  125000  -> 8 moth
-    -- 
+    --  bottel - > 1 hr -> 100
+    --  daily - > 24 hr -> 2400
+    --  monthly - > 24 hr -> 2150 
+    
+    --  bottel - > 1 hr -> 1000
+    --  daily - > 24 hr -> 24000
+    --  monthly - > 24 hr -> 22500
+    
+        -- 
+
+    INSERT INTO products VALUES
+    (1001,'Bottle','Container',10.5),
+    (1002,'Cap','Container',1.5),
+    (2001,'Rack','Holdings',125),
+    (2002,'Rack_stand','Holdings',8.5);
+
+    INSERT INTO machine VALUES
+    (1001,'Rx001-Semiauto','Line-A',150,'2026-07-07'),
+    (1002,'Rx002-menual','Line-B',90,'2026-07-07');
+
+    INSERT INTO production VALUES
+    (1101,1001,1001,'2026-07-07',2150,50,21.5),
+    (1102,1002,1002,'2026-07-07',22500,2500,22.5);
+
+      INSERT INTO maintainance VALUES
+      (1,1001,'2026-07-07','none',0,0),
+      (2,1002,'2026-07-07','none',0,0);
+
